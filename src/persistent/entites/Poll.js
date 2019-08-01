67e32fdd61de0bf9c68c8222bed7fb141bf1ace0
Vova Pihol <cetncat@gmail.com>
2019-08-01 21:36:08 +0300

    Edit Poll schema

--- src/persistent/entites/Poll.js
+++ src/persistent/entites/Poll.js
@@ -1,14 +1,20 @@
 const mongoose = require("../index");
 const nanoid = require("nanoid");
 
-const userSchema = new mongoose.Schema({
+const pollSchema = new mongoose.Schema({
   id: {type: String, default: nanoid()},
   question: String,
-  options: {type: Array},
-  active: {type: Boolean},
+  active: Boolean,
+  options: [{
+    id: {type: String, default: nanoid()},
+    value: String,
+    votes: {type: Number, default: 0}
+  }],
+  votes: {type: Number, default: 0},
+  endsAt: Date,
   createdAt: {type: Date, default: Date.now()}
 });
 
-const Poll = mongoose.model('Poll', userSchema);
+const Poll = mongoose.model('Poll', pollSchema);
 
 module.exports = Poll;
