5e96c4892994be49f18034f169c5dc82395b49ee
Vova Pihol <cetncat@gmail.com>
2019-08-20 19:51:43 +0300

    Add new tests

---
+++ tests/utils/LimitedMemoryQueue.test.js
@@ -0,0 +1,19 @@
+process.env.NODE_ENV = 'testing';
+
+const chai = require("chai");
+
+chai.should();
+
+const expect = chai.expect;
+
+describe("utils", () => {
+  describe("LimitedMemoryQueue.js", () => {
+    it("should return a html page", (done) => {
+
+    });
+
+    it("should return current version", (done) => {
+
+    });
+  });
+});
