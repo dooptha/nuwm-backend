50465870e0c8e5f28d4df724c7d12be6fc36fd7a
Vova Pihol <cetncat@gmail.com>
2019-09-05 20:20:17 +0300

    Add field for subgroups and other strange cases

--- src/services/entities/Timetable.js
+++ src/services/entities/Timetable.js
@@ -1,5 +1,5 @@
 class Subject {
-  constructor(data, lecturer) {
+  constructor(data, lecturer, requestGroup) {
     this.lecturer = data.lecturer || lecturer;
     this.group = data.subgroup;
     this.lesson = data.lessonNum;
@@ -7,25 +7,38 @@ class Subject {
     this.classroom = data.classroom;
     this.name = data.subject;
     this.type = data.type;
+    this.streamsType = data.streams_type;
+
+    this.setCustomFields(requestGroup);
+  }
+
+  setCustomFields(requestGroup) {
+    if (
+      this.group &&
+      this.streamsType !== 'Потік' &&
+      !this.group.includes(requestGroup)
+    )
+      this.displayGroup = this.group;
   }
 }
 
 class Day {
-  constructor(data, lecturer) {
+  constructor(data, lecturer, requestGroup) {
     this.date = data.day;
     this.dayName = data.dayname;
     this.day = data.day_of_week;
     this.dayOfYear = data.day_of_year;
     this.subjects = data.subjects
-      .map(subject => new Subject(subject, lecturer));
+      .map(subject => new Subject(subject, lecturer, requestGroup));
   }
 }
 
 class Timetable {
   constructor(body, params) {
-   this.lecturer = params.name;
-   this.schedule = body.response.schedule
-     .map(day => new Day(day, this.lecturer));
+    this.lecturer = params.name;
+    this.requestGroup = params.group;
+    this.schedule = body.response.schedule
+      .map(day => new Day(day, this.lecturer, this.requestGroup));
   }
 }
 
